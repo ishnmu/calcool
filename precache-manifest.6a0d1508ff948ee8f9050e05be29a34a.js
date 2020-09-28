7e7525557d0c69fe014b88a81da740d203481dce
Shunmugam V <v.shunmugam@thoughtworks.com>
2020-09-28 23:38:17 +0530

    Updates

---
+++ precache-manifest.6a0d1508ff948ee8f9050e05be29a34a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922888f3cca5f3996a54c1f8ebe2e023",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83ccbf4fcbeaceed8502",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ef5c2d14d9e23bfda206",
+    "url": "/static/js/2.f0f4c7ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f0f4c7ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ccbf4fcbeaceed8502",
+    "url": "/static/js/main.74bc10e0.chunk.js"
+  },
+  {
+    "revision": "3c3a0e66811293768c78",
+    "url": "/static/js/runtime-main.964fe35a.js"
+  }
+]);

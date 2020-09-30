c74aef80feaaf119300d4827aed0e268746a9035
ishnmu <sanjayshanmu7@gmail.com>
2020-10-01 01:30:14 +0530

    Shanmu | add styling without breaking responsiveness

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Calculator from "./components/Calculator/Calculator";
+import "./App.css";
 
 function App() {
-  return <Calculator />;
+  return (
+    <div className="container">
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Calculator/Calculator.style.js
+++ src/components/Calculator/Calculator.style.js
@@ -4,9 +4,8 @@ export const CalculatorWrapper = styled.div`
   display: flex;
   flex-direction: column;
   border: 1px solid black;
-  max-width: 100%;
-  max-height: 100%;
   padding: 1vh;
+  height: fit-content;
 `;
 
 export const Section = styled.div`

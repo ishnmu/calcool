716c28e89f442b69fb9990177986a91bb68d3517
ishnmu <sanjayshanmu7@gmail.com>
2020-10-02 14:06:08 +0530

    Shunmugam | add styling

--- src/components/Screen/Screen.style.js
+++ src/components/Screen/Screen.style.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const ScreenWrapper = styled.section`
   text-align: right;
   background: black;
+  border-radius: 1vh;
   color: white;
   min-height: 15vh;
   font-size: 2em;
